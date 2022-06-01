74b545e18530c2b08d362ba8952dacdaa94e0401
computingsquid <56894058+ComputingSquid@users.noreply.github.com>
2022-06-01 19:38:52 +0000

    Initial commit

---
+++ code/assets.js
@@ -0,0 +1,18 @@
+export default function loadAssets() {
+	loadSprite("bean", "sprites/bean.png");
+	loadSprite("ghosty", "sprites/ghosty.png");
+	loadSprite("spike", "sprites/spike.png");
+	loadSprite("grass", "sprites/grass.png");
+	loadSprite("prize", "sprites/jumpy.png");
+	loadSprite("apple", "sprites/apple.png");
+	loadSprite("portal", "sprites/portal.png");
+	loadSprite("coin", "sprites/coin.png");
+  loadSprite("meat", "sprites/meat.png");
+  loadSprite("cots", "sprites/cots.png");
+  loadSprite("sand", "sprites/sand.png");
+	loadSound("coin", "sounds/score.mp3");
+	loadSound("powerup", "sounds/powerup.mp3");
+	loadSound("blip", "sounds/blip.mp3");
+	loadSound("hit", "sounds/hit.mp3");
+	loadSound("portal", "sounds/portal.mp3");
+}
